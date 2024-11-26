c2bd777d85db0ac52aeec56f8b6c947bd1fcfcdd
narendra <narendra.infolative36@gmail.com>
2024-11-26 16:44:39 +0530

    V6 Design added for website

---
+++ ersNARENDRAOneDriveDesktopGitindex.php
@@ -0,0 +1,23 @@
+[33mcommit c903a30ef31de2d15f04eae699b03cdcf54bacea[m[33m ([m[1;36mHEAD[m[33m -> [m[1;32mmain[m[33m, [m[1;31morigin/main[m[33m)[m
+Author: narendra <narendra.infolative36@gmail.com>
+Date:   Tue Nov 26 14:13:37 2024 +0530
+
+    Version V4
+
+[33mcommit 5519078349994d5715e64e388cf096c638098eaa[m
+Author: narendra <narendra.infolative36@gmail.com>
+Date:   Tue Nov 26 12:37:21 2024 +0530
+
+    Version V3
+
+[33mcommit df1919f00ed8a8a454a52289a3f7182d2b536c97[m
+Author: narendra <narendra.infolative36@gmail.com>
+Date:   Tue Nov 26 12:33:14 2024 +0530
+
+    This is V2
+
+[33mcommit 14c68b8c8226dfeb0deee2949481713532e83468[m
+Author: narendra <narendra.infolative36@gmail.com>
+Date:   Tue Nov 26 12:23:44 2024 +0530
+
+    First version of index.php
